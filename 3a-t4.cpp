38c28c3698db651cdb9da8550c8d8f33f6c084eb
unanianmaria <93879143+unanianmaria@users.noreply.github.com>
2021-11-28 12:28:27 +0300

    Update 3a-t4.cpp

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -4,13 +4,18 @@ using namespace std;
 int main()
 {
 int k, i = 1, sum_k = 0, number;
-cout << " Enter the amount of integers to sum = "; cin >> k;
-do {
-cout << "Enter integer nr. " << i << ": "; 
+cout << " Enter the amount of integers to sum = "; cin >> k; //user enters how many times loop will  continue
+do { if (k == 0) 
+{
+    break;
+  }
+cout << "Enter integer nr. " << i << ": "; //user prompt
 cin >> number;
 sum_k += number; 
-i++;
-} while (i <= k);
+i++;// increase the index variable 
+} while (i <= k);//loop will continue until i !> k
+ 
 cout << " The total sum of " << k << " integers is: "  << sum_k;
 return 0;
 }
+
